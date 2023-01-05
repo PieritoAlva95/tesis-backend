98459305b808a15ae34efc35b7883594d4bb8910
Jean Alvarado <jean.alvarado795@gmail.com>
2023-01-04 21:11:31 -0500

    Fix: correcion en correos 7

--- controllers/oferta.js
+++ controllers/oferta.js
@@ -358,7 +358,7 @@ const actualizarOferta = async (req, res = response) => {
       usuario: uid,
     };
 
-    console.log(`CAMBIO_OFERTA: ${cambioOferta}`)
+    console.log(`CAMBIO_OFERTA: ${cambioOferta.body}`)
 
     const ofertaActualizado = await Oferta.findByIdAndUpdate(id, cambioOferta, {
       new: true,
@@ -368,65 +368,65 @@ const actualizarOferta = async (req, res = response) => {
           ok: false,
           msg: "Ha ocurrido un error en la actulizacion!",
         })
-      } else {
-        if (cambioOferta.interesados.aceptado === true) {
-          const idUsuarioContratado = cambioOferta.interesados.postulante;
-          console.log(`idUsuarioContratado: ${idUsuarioContratado}`)
-          const usuarioContratado = await Usuario.findById(idUsuarioContratado);
-          console.log(`usuarioContratado: ${usuarioContratado}`)
-          const usuarioQueContrata = await Usuario.findById(cambioOferta.usuario);
-          if (usuarioContratado && usuarioQueContrata) {
-            const emailUsuarioContratado = usuarioContratado.email;
-            console.log(`emailUsuarioContratado: ${emailUsuarioContratado}`)
-            const emailUsuarioQueContrata = usuarioQueContrata.email;
-            console.log(`emailUsuarioQueContrata: ${emailUsuarioQueContrata}`)
-            const celularUsuarioQueContrata = usuarioQueContrata.numeroDeCelular;
-            const config = {
-              host: 'smtp.gmail.com',
-              port: 587,
-              auth: {
-                user: 'workjobstesis@gmail.com',
-                pass: 'mslvplvwdeyxtyvq'
-              }
-            }
-
-            let mailOptions = {
-              to: emailUsuarioContratado,
-              from: 'workjobstesis@gmail.com',
-              subject: 'Notificación de contrato Jobs - Trabajos 24/7',
-              html: `
-                  <table border="0" cellpadding="0" cellspacing="0" width="600px" background-color="#2d3436" bgcolor="#2d3436">
-                    <tr height="200px">
-                      <td bgcolor="" width="600"px>
-                        <h1 style="color: #fff; text-align:center">Notificación de contrato</h1>
-                        <p style="color:#fff; text-align:center">
-                          <span style:"color: #fff">Nos place comunicarte que has sido contratado en la oferta </span><br>
-                          <span style:"color: #fff">con titulo: ${cambioOferta.titulo} publicada por ${cambioOferta.nombreUsuario}</span><br><br>
-                          <span style:"color: #fff">te puedes contactar mediante el correo ${emailUsuarioQueContrata}</span><br><br>
-                          <span style:"color: #fff"> o al numero de celular ${celularUsuarioQueContrata}</span><br><br>
-                          <span style:"color: #fff"><b>No te olvides de ingresar a la plataforma y revisar la seccion de contratos</b></span><br>
-                          <span style:"color: #fff"><b>Gracias por confiar en nosotros!</b></span><br>
-                        </p>
-                      </td>
-                    </tr>            
-                  </table>
-                  `,
-            };
-
-            const transport = nodemailer.createTransport(config);
-
-            transport.sendMail(mailOptions, (err) => {
-              if (err) {
-                res.status(404).json({
-                  ok: false,
-                  msg: `Ha ocurrido un problema en el envio del correo. Error: ${err}`,
-                });
-              }
-            });
-          }
-        }
       }
     });
+    
+    if (cambioOferta.interesados.aceptado === true) {
+      const idUsuarioContratado = cambioOferta.interesados.postulante;
+      console.log(`idUsuarioContratado: ${idUsuarioContratado}`)
+      const usuarioContratado = await Usuario.findById(idUsuarioContratado);
+      console.log(`usuarioContratado: ${usuarioContratado}`)
+      const usuarioQueContrata = await Usuario.findById(cambioOferta.usuario);
+      if (usuarioContratado && usuarioQueContrata) {
+        const emailUsuarioContratado = usuarioContratado.email;
+        console.log(`emailUsuarioContratado: ${emailUsuarioContratado}`)
+        const emailUsuarioQueContrata = usuarioQueContrata.email;
+        console.log(`emailUsuarioQueContrata: ${emailUsuarioQueContrata}`)
+        const celularUsuarioQueContrata = usuarioQueContrata.numeroDeCelular;
+        const config = {
+          host: 'smtp.gmail.com',
+          port: 587,
+          auth: {
+            user: 'workjobstesis@gmail.com',
+            pass: 'mslvplvwdeyxtyvq'
+          }
+        }
+
+        let mailOptions = {
+          to: emailUsuarioContratado,
+          from: 'workjobstesis@gmail.com',
+          subject: 'Notificación de contrato Jobs - Trabajos 24/7',
+          html: `
+              <table border="0" cellpadding="0" cellspacing="0" width="600px" background-color="#2d3436" bgcolor="#2d3436">
+                <tr height="200px">
+                  <td bgcolor="" width="600"px>
+                    <h1 style="color: #fff; text-align:center">Notificación de contrato</h1>
+                    <p style="color:#fff; text-align:center">
+                      <span style:"color: #fff">Nos place comunicarte que has sido contratado en la oferta </span><br>
+                      <span style:"color: #fff">con titulo: ${cambioOferta.titulo} publicada por ${cambioOferta.nombreUsuario}</span><br><br>
+                      <span style:"color: #fff">te puedes contactar mediante el correo ${emailUsuarioQueContrata}</span><br><br>
+                      <span style:"color: #fff"> o al numero de celular ${celularUsuarioQueContrata}</span><br><br>
+                      <span style:"color: #fff"><b>No te olvides de ingresar a la plataforma y revisar la seccion de contratos</b></span><br>
+                      <span style:"color: #fff"><b>Gracias por confiar en nosotros!</b></span><br>
+                    </p>
+                  </td>
+                </tr>            
+              </table>
+              `,
+        };
+
+        const transport = nodemailer.createTransport(config);
+
+        transport.sendMail(mailOptions, (err) => {
+          if (err) {
+            res.status(404).json({
+              ok: false,
+              msg: `Ha ocurrido un problema en el envio del correo. Error: ${err}`,
+            });
+          }
+        });
+      }
+    }
     console.log(`ofertaActualizado: ${ofertaActualizado}`)
     res.json({
       ok: true,
